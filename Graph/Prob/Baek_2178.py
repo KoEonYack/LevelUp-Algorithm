0bc835e462fa72696e3d188a3221b2ab40c4e6a3
KoEonYack <21300035@handong.edu>
2019-09-19 21:45:51 +0900

    Add| Baek 2178

---
+++ Graph/Prob/Baek_2178.py
@@ -0,0 +1,18 @@
+'''
+    @ 2178. 미로탐색
+    @ Prob. https://www.acmicpc.net/problem/2178
+     Ref. https://jun-itworld.tistory.com/20?category=778547
+    @ Algo: BFS
+    @ Start day: 19. 09. 19
+    @ End day:
+'''
+
+
+N, M = map(int, input().split())
+map = [[]*N]
+
+for i in range(N):
+    print ( map(int, input().split()) )
+    # map[i].append( )
+
+print(map)
