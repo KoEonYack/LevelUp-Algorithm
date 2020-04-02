014c63afcbb72b4fc9b6ae8d0e9f591c5bbdc44e
KoEonYack <21300035@handong.edu>
2020-04-02 21:02:53 +0900

    Add baek

--- DP/Baek_12847.py
+++ DP/Baek_12847.py
@@ -11,17 +11,19 @@
 N, M = map(int, input().split())
 arr = [0] + list(map(int, input().split()))
 res = sum(arr[1:M+1])
+maxV = res
 #print(res)
 for i in range(M+1, N+1):
     # print(i)
-    if res < res - arr[i] + arr[i-M]:
-        res = res - arr[i] + arr[i-M]
+    res = res + arr[i] - arr[i-M]
+    if maxV < res:
+        maxV = res
 
-print(res)
+print(maxV)
 
 """
-5 3
-10 20 30 20 10
+6 6
+10 20 30 20 10 90
 >
 70
 """
--- Others/Implementation/Baek_2482.py
+++ Others/Implementation/Baek_2482.py
@@ -8,24 +8,28 @@
     @ End day: 20. 03. 30.
 """
 
-def NCK(n, k):
-    numerator = 1
-    denominator = 1
-    k = min(n-k, k)
-    for i in range(1, k+1):
-        denominator *= i
-        numerator *= n+1-i
-    return numerator/denominator
-
-
 mod = 1000000003
 N = int(input())
 K = int(input())
-res = 0
 
-trp = 0
-for i in range(K):
-    NCK(N-trp, K)
+DP = [[0] * 1001 for _ in range(1001)]
+
+for i in range(N+1):
+    DP[i][1] = i
+    DP[i][0] = 1
 
 
+for i in range(2, N+1):
+    for j in range(2, K+1):
+        DP[i][j] = (DP[i-2][j-1] + DP[i-1][j]) % mod
+
+print((DP[N-1][K] + DP[N-3][K-1]) % mod)
+
+
+"""
+4
+2
+>
+2
+"""
 
