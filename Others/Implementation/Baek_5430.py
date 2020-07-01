439ac3031f400c34239c359a977c35b613859209
KoEonYack <21300035@handong.edu>
2020-07-01 21:28:36 +0900

    Add baek 5430

--- Others/Implementation/Baek_5430.py
+++ Others/Implementation/Baek_5430.py
@@ -1,6 +1,6 @@
 """
     @ Baek 5430. AC
-    @ Prob. https://www.acmicpc.net/problem/5439
+    @ Prob. https://www.acmicpc.net/problem/5430
      Ref.
     @ Algo: Implement
     @ Start day: 20. 06. 30.
@@ -16,37 +16,44 @@ for _ in range(T):
     L = int(input())
     elements = deque(input()[1:-1].split(","))
     isReversed = False
-    print("------------------")
-    print(elements, type(elements))
+    # print(elements, type(elements))
 
     flag = False
     for cmd in cmds:
         if cmd == "R":
-            # isReversed = False if isReversed else True
-            if isReversed:
-                isReversed = False
-            else:
-                isReversed = True
+            isReversed = False if isReversed else True
+            # if isReversed:
+            #     isReversed = False
+            # else:
+            #     isReversed = True
         elif cmd == "D":
             if L == 0:
-                print("error")
                 flag = True
                 break
-            elif isReversed:
+            elif isReversed is True:
                 elements.pop()
                 L -= 1
-            elif isReversed is True:
+                # print("DEBUG1", elements)
+            elif isReversed is False:
                 elements.popleft()
-                L -= 1
+                L -=1
+                # print("DEBUG2", elements)
 
-    if flag:
+
+    if flag is True:
         print("error")
     else:
-        if isReversed:
-            print(elements)
-        elif isReversed:
-            print(elements)
+        if isReversed is True:
+            elements.reverse()
+            print("[", end="")
+            print(",".join(list(elements)), end="")
+            print("]")
+        elif isReversed is False:
+            print("[", end="")
+            print(",".join(list(elements)), end='')
+            print("]")
     flag = False
+
 
 """
 4
@@ -67,4 +74,9 @@ D
 error
 [1,2,3,5,8]
 error
+
+1
+RRD
+6
+[1,1,2,3,5,8]
 """
