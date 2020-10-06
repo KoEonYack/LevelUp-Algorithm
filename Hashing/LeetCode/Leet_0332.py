0f3de290db5401140ed0d3e1ab81809f83a1b9d9
KoEonYack <21300035@handong.edu>
2020-10-06 12:37:59 +0000

    Add Leet code

--- Hashing/LeetCode/Leet_0332.py
+++ Hashing/LeetCode/Leet_0332.py
@@ -7,10 +7,28 @@
     @ End day: 20. 10. 05.
 """
 
+from typing import List
+from collections import defaultdict
 
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        graph = defaultdict()
+        for src, des in tickets:
+            if not graph.get(src, None):
+                graph[src] = [des]
+            else:
+                graph[src].append(des)
         
+        graph = [graph[k].sort() for k in graph.keys()]
+        visited = []
+        def dfs(src):
+            while graph.get(src, None):
+                des = graph[src].pop(0)
+                dfs(des)
+            visited.append(src)
+            
+        dfs('JFK')
+        return visited[::-1]
         
         
         
---
+++ LeetCode/Leet_0347.py
@@ -0,0 +1,29 @@
+"""
+    @ LeetCode 0347. Top K Frequent Elements
+    @ Prob. https://leetcode.com/problems/top-k-frequent-elements/
+      Ref.
+    @ Algo: 
+    @ Start day: 20. 10. 06.
+    @ End day: 20. 10. 06.
+"""
+
+from typing import List
+from collections import Counter
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+
+
+        Counter().most_common()
+        
+        
+if __name__ == "__main__":
+    nums = [1,1,1,2,2,3]
+    k = 2
+    solution = Solution(nums, KeyboardInterrupt)
+    
+"""
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+"""
+        
